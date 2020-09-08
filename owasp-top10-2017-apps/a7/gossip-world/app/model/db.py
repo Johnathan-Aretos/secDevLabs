afcd7abb0878a045a146134c7216cc56e8485b84
Johnathan <johno.a7@gmail.com>
2020-09-08 21:38:17 +1000

    vuln librariest

--- owasp-top10-2017-apps/a7/gossip-world/app/model/db.py
+++ owasp-top10-2017-apps/a7/gossip-world/app/model/db.py
@@ -22,8 +22,11 @@ class DataBase:
 
     def get_user_password(self, username):
         try:
+            # self.c.execute(
+            #     'SELECT password FROM users WHERE user = %s', [username])
+            # user_password = self.c.fetchone()
             self.c.execute(
-                'SELECT password FROM users WHERE user = %s', [username])
+                f"SELECT password FROM users WHERE user = '{username}'")
             user_password = self.c.fetchone()
 
         except (AttributeError, MySQLdb.OperationalError):
@@ -84,12 +87,12 @@ class DataBase:
     def search_gossips(self, search_str):
         try:
             self.c.execute(
-                'SELECT id, text, author, title, subtitle, date FROM gossips WHERE title  LIKE %s', ['%'+ search_str + '%'])
+                'SELECT id, text, author, title, subtitle, date FROM gossips WHERE title  LIKE %s', ['%' + search_str + '%'])
             gossips = self.c.fetchall()
         except (AttributeError, MySQLdb.OperationalError):
             self.connect()
             self.c.execute(
-                'SELECT id, text, author, title, subtitle, date FROM gossips WHERE title  LIKE %s', ['%'+ search_str + '%'])
+                'SELECT id, text, author, title, subtitle, date FROM gossips WHERE title  LIKE %s', ['%' + search_str + '%'])
             gossips = self.c.fetchall()
         except MySQLdb.Error as e:
             try:
@@ -185,46 +188,46 @@ class DataBase:
         return '', 1
 
     def init_table_user(self):
+        try:
+            self.c.execute(
+                'CREATE TABLE users (user VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL)')
+            self.db.commit()
+        except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
+            self.connect()
             try:
-                self.c.execute(
-                    'CREATE TABLE users (user VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL)')
-                self.db.commit()
-            except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
-                self.connect()
-                try:
-                    message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
-                    return message, 0
-                except IndexError:
-                    message = 'MySQL Error: %s' % str(e)
-                    return message, 0
-            return '', 1
+                message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
+                return message, 0
+            except IndexError:
+                message = 'MySQL Error: %s' % str(e)
+                return message, 0
+        return '', 1
 
     def init_table_gossips(self):
+        try:
+            self.c.execute(
+                'CREATE TABLE gossips (id INT(10) NOT NULL AUTO_INCREMENT, author VARCHAR(100) NOT NULL, text VARCHAR(2000) NOT NULL, title VARCHAR(100) NOT NULL, subtitle VARCHAR(200), date DATE NOT NULL, PRIMARY KEY (id))')
+            self.db.commit()
+        except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
+            self.connect()
             try:
-                self.c.execute(
-                    'CREATE TABLE gossips (id INT(10) NOT NULL AUTO_INCREMENT, author VARCHAR(100) NOT NULL, text VARCHAR(2000) NOT NULL, title VARCHAR(100) NOT NULL, subtitle VARCHAR(200), date DATE NOT NULL, PRIMARY KEY (id))')
-                self.db.commit()
-            except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
-                self.connect()
-                try:
-                    message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
-                    return message, 0
-                except IndexError:
-                    message = 'MySQL Error: %s' % str(e)
-                    return message, 0
-            return '',
+                message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
+                return message, 0
+            except IndexError:
+                message = 'MySQL Error: %s' % str(e)
+                return message, 0
+        return '',
 
     def init_table_comments(self):
+        try:
+            self.c.execute(
+                'CREATE TABLE comments (author VARCHAR(100) NOT NULL, comment VARCHAR(100) NOT NULL, gossip_id INT NOT NULL, date DATE NOT NULL)')
+            self.db.commit()
+        except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
+            self.connect()
             try:
-                self.c.execute(
-                    'CREATE TABLE comments (author VARCHAR(100) NOT NULL, comment VARCHAR(100) NOT NULL, gossip_id INT NOT NULL, date DATE NOT NULL)')
-                self.db.commit()
-            except (AttributeError, MySQLdb.OperationalError, MySQLdb.Error) as e:
-                self.connect()
-                try:
-                    message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
-                    return message, 0
-                except IndexError:
-                    message = 'MySQL Error: %s' % str(e)
-                    return message, 0
-            return '',
+                message = 'MySQL Error [%d]: %s' % (e.args[0], e.args[1])
+                return message, 0
+            except IndexError:
+                message = 'MySQL Error: %s' % str(e)
+                return message, 0
+        return '',
